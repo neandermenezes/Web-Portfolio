b6b22327a9c0c4c495f36e0755bba356bf450a0d
neandermenezes <neander2801@gmail.com>
2022-03-01 13:10:32 -0300

    mobile v2.0

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects() {
   return (
     <div id="projects" className="projects-section">
       {ProjectsData.map(({ title, description, image, tools, repository }, index) => (
-        <div key={title} data-aos={(index + 1) % 2 === 0 ? "fade-left" : "fade-right"} className={(index + 1) % 2 === 0 ? "project-invert" : "project"}>
+        <div key={title} className={(index + 1) % 2 === 0 ? "project-invert" : "project"}>
           <div className="project__card">
             <h3 className="project__title">{title}</h3>
             <p className="project__description">{description}</p>
